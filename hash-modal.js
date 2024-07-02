aa42ed8799ef22c8a5b3766dd3bfa6eeadda5eed
PBorzov <p.borzov@gmail.com>
2024-07-03 02:02:29 +0300

    init

---
+++ hash-modal.js
@@ -0,0 +1,25 @@
+for (let dialog of document.querySelectorAll("dialog")) {
+    if (dialog.dataset.handler === "hash-modal") {
+        let dialogButtonClose = dialog.querySelector(".hash-modal-close");
+        dialog.addEventListener('click', (e) => { 
+            console.log(dialog, e.target)
+            if (dialog == e.target) dialog.close(); 
+        });
+        dialogButtonClose.addEventListener('click', () => { dialog.close(); })
+        dialog.addEventListener("close", () => { history.pushState("", document.title, window.location.pathname + window.location.search); })
+    }
+}
+
+function hashWatcher() {
+    if (location.hash) {
+        let hash = location.hash.replace("#", "");
+        let dialog = document.getElementById(hash);
+        if (dialog.dataset.handler !== "hash-modal") return;
+        dialog.showModal();
+        return;
+    }
+    return;
+}
+
+window.addEventListener('DOMContentLoaded', hashWatcher)
+window.addEventListener('hashchange', hashWatcher);
